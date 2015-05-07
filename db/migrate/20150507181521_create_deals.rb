b48a4d4e583f2a02509789202eac3b1556d0140c
Mark Smukler <smukler.mark@gmail.com>
2015-05-07 15:01:13 -0400

    creating migrations for users and deals

---
+++ db/migrate/20150507181521_create_deals.rb
@@ -0,0 +1,6 @@
+class CreateDeals < ActiveRecord::Migration
+  def change
+    create_table :deals do |t|
+    end
+  end
+end
---
+++ db/migrate/20150507181543_deals_users.rb
@@ -0,0 +1,4 @@
+class DealsUsers < ActiveRecord::Migration
+  def change
+  end
+end
