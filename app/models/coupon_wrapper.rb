6decf36652224946226c0333cd5f8c6fc21d2f42
Mark Smukler <smukler.mark@gmail.com>
2015-05-08 17:06:12 -0400

    built out first login & signup modal views, added css

--- app/models/coupon_wrapper.rb
+++ app/models/coupon_wrapper.rb
@@ -8,7 +8,7 @@ class CouponWrapper
 	limit = '1000'
 	order = 'popular'
 
-	def self.get_deals
+	def self.all
 		search_url = [
 			API_ROOT_URL,
 			'getdeals?',
@@ -25,7 +25,35 @@ class CouponWrapper
 
 		response = HTTParty.get(search_url)
 
-		response
+		response.each do |deal|
+			Deal.create(
+	    	biz_name: response.name,
+	    	biz_address: response.address,
+	    	biz_city: response.city,
+	    	biz_state: response.state,
+	    	biz_zip: response.zip,
+	    	biz_phone: response.phone,
+	    	deal_url: response.URL,
+	    	deal_id: response.ID,
+	    	deal_title: response.dealTitle,
+	    	deal_desc: response.dealinfo,
+	    	deal_disclaim: response.disclaimer,
+	    	expiration: response.expirationDate,
+	    	deal_image: response.showImage,
+	    	deal_image_big: response.showImageStandardBig,
+	    	deal_image_small: response.showImageStandardSmall,
+	    	logo_image: response.showLogo,
+	    	category: response.categoryID,
+	    	subcategory: response.subcategoryID,
+	    	provider: response.providerName,
+	    	deal_price: response.dealPrice,
+	    	original_price: response.dealOriginalPrice,
+	    	savings_amount: response.dealSavings,
+	    	savings_percent: response.dealDiscountPercent,
+	    	total_num_deals: response.totalDealsInThisStore
+			)
+		end
+
 	end
 
 
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+
+	has_secure_password # I dont know if i need this / what it does -- took from weather_app
+	has_and_belongs_to_many :deals
+
+	validates :username, :password_digest,
+						presence: true
+						uniqueness: { case_sensitive: false }
+
+	validates :first_name, :last_name, :zipcode,
+						presence: true
+
+end
